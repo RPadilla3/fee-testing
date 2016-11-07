f74cd8c37a233f71c4d9ff628c00950d1ae98ceb
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-11-07 16:14:57 -0500

    Updated testing file.

--- test/specs/calc.spec.js
+++ test/specs/calc.spec.js
@@ -1,35 +1,44 @@
 (function() {
     'use strict';
 
-    window.calc = window.calc || {};
+        window.calc = window.calc || {};
 
-    // TODO: Your tests go here!
-    var expect = chai.expect;
+        // TODO: Your tests go here!
+        var expect = chai.expect;
 
 
-    describe('My test', function(){
-      it('Should get the result of the function from added numbers in the array.', function(){
-        var result = window.calc.sum([3,7,9]);
-        expect(result).to.equal(19);
-      });
+        describe('Array test', function() {
+            it('Should get the result of the function from added numbers in the array.', function() {
+                var result = window.calc.sum([3, 7, 9]);
+                expect(result).to.equal(19);
+            });
 
-        it('Should determine if there are no arguments it is equal to 0.', function(){
-          var result = window.calc.sum();
-          expect(result).to.equal(0);
+            it('Should determine if there are no arguments it is equal to 0.', function() {
+                var result = window.calc.sum();
+                expect(result).to.equal(0);
+            });
+
+            it('Should determine if the argument is a string the result is a string.', function() {
+                var result = window.calc.sum('Rodolfo');
+                expect(result).to.equal('Rodolfo');
+            })
+
         });
 
-        it('Should determine if the argument is a string it is undefined', function(){
-          var result = window.calc.sum('Rodolfo');
-          expect(result).to.equal('undefined');
-        })
-      });
 
-    xdescribe('Basic test', function(){
+        // describe('Factorial Function Test', function() {
+        //         it('Should multiply numbers together up to a chosen factorial', function({
+        //           var factorial = window.calc.factorial(5);
+        //           expect(factorial).to.be.a('function');
+        //     })
+        // });
 
-      it('Should know that 1 is equal to 1.', function(){
-        expect(1).to.equal(1);
-      });
-    });
+        xdescribe('Basic test', function() {
+
+                it('Should know that 1 is equal to 1.', function() {
+                    expect(1).to.equal(1);
+                });
+            });
 
 
 })();
