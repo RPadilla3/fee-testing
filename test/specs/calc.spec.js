4de36810f51106565770705c49bfb5765cf76f80
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-11-07 15:13:59 -0500

    Created a test to find out if the function will print out undefined if a string is put in to the array.

--- test/specs/calc.spec.js
+++ test/specs/calc.spec.js
@@ -6,22 +6,27 @@
     // TODO: Your tests go here!
     var expect = chai.expect;
 
-    
+
     describe('My test', function(){
-      it('Should get the result of the function from added numbers in the array', function(){
+      it('Should get the result of the function from added numbers in the array.', function(){
         var result = window.calc.sum([3,7,9]);
         expect(result).to.equal(19);
       });
 
-        it('Should determine if there are no arguments it is undefined', function(){
+        it('Should determine if there are no arguments it is equal to 0.', function(){
           var result = window.calc.sum();
-          expect(result).to.equal(undefined);
+          expect(result).to.equal(0);
         });
+
+        it('Should determine if the argument is a string it is undefined', function(){
+          var result = window.calc.sum('Rodolfo');
+          expect(result).to.equal('undefined');
+        })
       });
 
-    describe('Basic test', function(){
+    xdescribe('Basic test', function(){
 
-      it('Should know that 1 is equal to 1', function(){
+      it('Should know that 1 is equal to 1.', function(){
         expect(1).to.equal(1);
       });
     });
